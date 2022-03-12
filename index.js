e0f9505b6e0a86ba2e39610e62d14752d3d23807
Lilas-w <wangziweipku@pku.edu.cn>
2022-03-13 00:19:38 +0800

    update phonebook backend 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let data = [
     {
         "id": 1,
@@ -26,6 +28,70 @@ let data = [
 
 app.get('/api/persons', (req, res) => {
     res.json(data)
+})
+
+app.get('/info', (req, res) => {
+    const body = `
+    <p>Phonebook has info for ${data.length} people</p>
+    <p>${new Date()}</p>
+    `
+    res.send(body)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    let result = data.find(num => num.id === Number(id))
+    if (result) {
+        res.json(result)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    data = data.filter(num => num.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = Math.max(...data.map(num => num.id))
+    return Math.floor(Math.random() * 100000) + maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'      //必须使用return，否则代码执行到最后才能返回格式不正确的person保存
+        })
+    }
+
+    if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'      //必须使用return，否则代码执行到最后才能返回格式不正确的person保存
+        })
+    }
+
+    const existed = data.find(person => person.name === body.name)
+
+    if (existed) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number || 0
+    }
+
+    data = data.concat(person)
+
+    res.json(person)
 })
 
 const PORT = 3001
